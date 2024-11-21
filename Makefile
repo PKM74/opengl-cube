8d1adbcc254eebdb1c9a7a3f3c73dfaf54a9617d
Tyler McGurrin <mcgurrintyler@gmail.com>
2024-11-21 14:28:09 -0500

    Balh balh blah

---
+++ Makefile
@@ -0,0 +1,16 @@
+CC=gcc
+LIB_DIR=lib
+BUILD_DIR=build
+SRC_DIR=src
+.PHONY: all always clean
+
+all: main
+
+main: always
+	$(CC) -g $(SRC_DIR)/main.c -o $(BUILD_DIR)/main
+	chmod +x $(BUILD_DIR)/main
+always:
+	mkdir -p $(BUILD_DIR)
+
+clean:
+	rm -rf $(BUILD_DIR)
---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*==============*\
+| Copyright 2024 |
+| Tyler McGurrin |
+\*==============*/
+
+//include files
+#include <stdio.h>
+#include <stdint.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+//Definitions and Vars
+#define true 1;
+#define false 0;
+
+
+int main()
+{
+    glutInit;
+    return true;
+}
